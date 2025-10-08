2bd5652b80a588249e614e795daedc09442fa0b0
Julian Lobbes <julian@lobbes.dev>
2025-10-08 06:01:19 +0100

    refactor: split into separate functions

--- batch_qrs.go
+++ batch_qrs.go
@@ -13,36 +13,47 @@ import (
 func main() {
 	var args = os.Args[1:]
 	if len(args) < 1 {
-		fmt.Fprintf(os.Stderr, "ERROR: at least one argument is required\n")
-		os.Exit(1)
+		errorExit("at least one argument is required")
 	}
 
 	for _, arg := range args {
-		qrc, err := qrcode.NewWith(
-			arg,
-			qrcode.WithEncodingMode(qrcode.EncModeByte),
-			qrcode.WithErrorCorrectionLevel(qrcode.ErrorCorrectionMedium),
-		)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "ERROR: %s\n", err)
-			os.Exit(1)
-		}
-
-		buf := bytes.NewBuffer(nil)
-		wr := nopCloser{Writer: buf}
-		w2 := standard.NewWithWriter(
-			wr,
-			standard.WithQRWidth(10),
-			standard.WithBuiltinImageEncoder(standard.PNG_FORMAT),
-			standard.WithBorderWidth(20),
-		)
-		if err = qrc.Save(w2); err != nil {
-			fmt.Fprintf(os.Stderr, "ERROR: %s\n", err)
-			os.Exit(1)
-		}
-
-		fmt.Printf("%s\n", base64.StdEncoding.EncodeToString(buf.Bytes()))
+		bytes := generateQrCode(arg)
+		fmt.Println(base64Encode(bytes))
 	}
+}
+
+func errorExit(message string) {
+	fmt.Fprintf(os.Stderr, "ERROR: %s\n", message)
+	os.Exit(1)
+}
+
+func generateQrCode(payload string) []byte {
+	qrc, err := qrcode.NewWith(
+		payload,
+		qrcode.WithEncodingMode(qrcode.EncModeByte),
+		qrcode.WithErrorCorrectionLevel(qrcode.ErrorCorrectionMedium),
+	)
+	if err != nil {
+		errorExit(err.Error())
+	}
+
+	buf := bytes.NewBuffer(nil)
+	wr := nopCloser{Writer: buf}
+	w2 := standard.NewWithWriter(
+		wr,
+		standard.WithQRWidth(10),
+		standard.WithBuiltinImageEncoder(standard.PNG_FORMAT),
+		standard.WithBorderWidth(20),
+	)
+	if err = qrc.Save(w2); err != nil {
+		errorExit(err.Error())
+	}
+
+	return buf.Bytes()
+}
+
+func base64Encode(payload []byte) string {
+	return base64.StdEncoding.EncodeToString(payload)
 }
 
 type nopCloser struct {
